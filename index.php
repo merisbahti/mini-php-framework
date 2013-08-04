f585dbe3b81dc1b0f7b378f96651b35a1c7df4bf
3amice <bahtijaragic@gmail.com>
2013-08-04 21:08:35 +0200

    add rb test

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 require 'vendor/autoload.php';
 #Loads & initialises RedBeanPHP
 require 'rb/rb.php';
+
 R::setup("sqlite:db.db");
+
+$bean 				= R::dispense('kontakter');
+$bean['name'] = "Tim";
+$id 					= R::store($bean);
+//echo R::load('kontakter',$id)->telNr;
 
 $app = new \Slim\Slim();
 $loader = new Twig_Loader_Filesystem('./templates');
@@ -11,6 +17,10 @@ $twig = new Twig_Environment($loader);
 
 $app->get('/hello/:name', function ($name) use ($twig) {
     echo $twig->render('index_example.html', array('name' => $name));
+  });
+
+$app->get('/hello/', function () use ($twig) {
+    echo $twig->render('index_example.html', array('name' => ""));
   });
 
 $app->run();
