daec60d85924a706f4413fd5b276669a727440ce
Meris Bahti <bahtijaragic@gmail.com>
2013-08-04 18:51:12 +0200

    add template example

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
+#Loads twig & slim
 require 'vendor/autoload.php';
+#Loads & initialises RedBeanPHP
 require 'rb/rb.php';
 R::setup("sqlite:db.db");
 
 $app = new \Slim\Slim();
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
 
-$app->get('/hello/:name', function ($name) {
-        echo "Hello, $name";
-        });
+$app->get('/hello/:name', function ($name) use ($twig) {
+    echo $twig->render('index_example.html', array('name' => $name));
+  });
 
 $app->run();
-
